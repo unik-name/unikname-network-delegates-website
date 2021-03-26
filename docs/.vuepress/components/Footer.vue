2e3492a7965f67cbdb503b77b7a3c742ccbf30eb
qsvtr <quentin.sauvetre@protonmail.com>
2021-03-26 18:21:53 +0100

    feat: init vuepress

---
+++ docs/.vuepress/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>
+    © 2021 <a href="https://www.unik-name.com">Unikname</a>&nbsp;&bullet;&nbsp;<a href="/powered-by-ark-io">Powered by ARK</a>
+  </footer>
+</template>
+
+<style lang="stylus" scoped>
+// css/stylus here or in global stylus file
+</script>
---
+++ docs/.vuepress/components/un.vue
@@ -0,0 +1,45 @@
+<template>
+  <span>
+    <span v-if="label">@unikname ID: </span>
+    <span class="unikname"><a :href="href()">{{ id }}&nbsp;&nbsp;<img src="/chainlink.svg" /></a></span>
+  </span>
+</template>
+
+<script>
+export default {
+    props: {
+      id: {type: String},
+      unikid: {type: String},
+      label: {type: Boolean},
+    },
+    methods: {
+      href: function() {
+        let xhref = "https://explorer.uns.network/uniks/";
+        if (!this.unikid || 0 === this.unikid.length) {
+          // xhref = "https://unikname.id/" + this.id
+          xhref += this.id;
+        } else {
+          xhref += this.unikid;
+        }
+        return xhref;
+      }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.unikname
+  background-color: #f6f8fa
+  color: #293984
+  font-weight: 500
+  margin: 1px 
+  padding: 0px 10px 2px 8px 
+  border-radius: 5px
+  border-style: solid 
+  border-width: 0.5px
+  a
+    color: #293984    
+
+</style>
+
