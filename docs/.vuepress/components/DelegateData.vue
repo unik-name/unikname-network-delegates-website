26d881cce1539dfa103acaf8256a981e06bcdcca
Damien Lecan <damien.lecan@spacelephant.org>
2021-06-03 15:47:47 +0200

    fix: allow to use extra plugins such as SEO

--- docs/.vuepress/components/DelegateData.vue
+++ docs/.vuepress/components/DelegateData.vue
@@ -1,90 +1,169 @@
-
 <template>
-<div>
+  <div>
     <span class="card-container">
-            <div v-for="delegate in delegates">
-                <a :href="`/delegates/${delegate.unikid}`">
-                <!-- redirect to README.md if not claimed -->
-                    <div class="card">
-                        <div class="card-header">
-                            <div v-if="delegate.notCompleted">
-                                <img height="100" width="100" alt="logo" :src="require(`@assets/default-logo.png`)">
-                            </div>
-                            <div v-else>
-                                <img height="100" width="100" alt="logo" :src="require(`@delegates/${delegate.unikid}/logo.png`)">
-                            </div>
-                        </div>
-                        <div class="text-content">
-                            <div class="unikid">
-                                <h3 style="color: black"> @{{ delegate.unikname }} </h3>
-                                <span v-if="delegate.type" :class="`unik-badge unik-badge-${delegate.type}`">
-                                        <img :src="require(`@assets/logo-${delegate.type}.png`)" alt="" class="unik-view-logo" />
-                                        {{ delegate.type }}
-                                </span>
-                            </div>
-                            <div v-if="!delegate.notCompleted" class="socials">
-                                <p v-if="delegate.twitter"><a target="_blank" :href="`https://twitter.com/${delegate.twitter}`"><i class="fa fa-twitter" />twitter</a></p>
-                                <p v-if="delegate.github"><a target="_blank" :href="`https://github.com/${delegate.github}`"><i class="fa fa-github" />github</a></p>
-                                <p v-if="delegate.email"><a target="_blank" :href="`mailto:${delegate.email}`"><i class="fa fa-envelope" />email</a></p>
-                                <p v-if="delegate.website"><a target="_blank" :href="delegate.website"><i class="fa fa-globe" />website</a></p>
-                                <p v-if="delegate.forum"><a target="_blank" :href="`https://forum.unikname.com/u/${delegate.forum}/summary`"><i class="fa fa-globe" />forum</a></p>
-                            </div>
-                            <div v-else>
-                                <p>
-                                    <a target="_blank" href="https://github.com/unik-name/uns-delegates-website/blob/master/README.md">
-                                     👉 Claim this delegate profile</a>
-                                 </p>
-                            </div>
-                            <div v-if="!loading" class="description">
-                                <p>rank: {{delegate.rank}} / votes: {{delegate.votes}}%</p>
-                                <span v-if="delegate.forger">elected: 
-                                    <img :src="require(`@assets/check.svg`)" height="15px" width="15px" alt="status"/>
-                                    <p>status:<span :class="`status-${delegate.isLive}`">{{delegate.isLive}}</span></p>
-                                </span>
-                                <span v-else>elected: 
-                                    <img :src="require(`@assets/cross.svg`)" height="15px" width="15px" alt="status"/>
-                                </span>
-                            </div>
-                            <div v-else class="placeholder shimmer">
-                                <div class="fake-text medium"/>
-                                <div class="fake-text short"/>
-                                <div class="fake-text"/>
-                            </div>
-                        </div>
-                    </div>
-                </a>
+      <div v-for="delegate in delegates" :key="delegate.unikid">
+        <a :href="`/delegates/${delegate.unikid}`">
+          <!-- redirect to README.md if not claimed -->
+          <div class="card">
+            <div class="card-header">
+              <div v-if="delegate.notCompleted">
+                <img
+                  height="100"
+                  width="100"
+                  alt="logo"
+                  :src="require(`@assets/default-logo.png`)"
+                />
+              </div>
+              <div v-else>
+                <img
+                  height="100"
+                  width="100"
+                  alt="logo"
+                  :src="require(`@delegates/${delegate.unikid}/logo.png`)"
+                />
+              </div>
             </div>
+            <div class="text-content">
+              <div class="unikid">
+                <h3 style="color: black">@{{ delegate.unikname }}</h3>
+                <span
+                  v-if="delegate.type"
+                  :class="`unik-badge unik-badge-${delegate.type}`"
+                >
+                  <img
+                    :src="require(`@assets/logo-${delegate.type}.png`)"
+                    alt=""
+                    class="unik-view-logo"
+                  />
+                  {{ delegate.type }}
+                </span>
+              </div>
+              <div v-if="!delegate.notCompleted" class="socials">
+                <p v-if="delegate.twitter">
+                  <a
+                    target="_blank"
+                    :href="`https://twitter.com/${delegate.twitter}`"
+                    ><i class="fa fa-twitter" />twitter</a
+                  >
+                </p>
+                <p v-if="delegate.github">
+                  <a
+                    target="_blank"
+                    :href="`https://github.com/${delegate.github}`"
+                    ><i class="fa fa-github" />github</a
+                  >
+                </p>
+                <p v-if="delegate.email">
+                  <a target="_blank" :href="`mailto:${delegate.email}`"
+                    ><i class="fa fa-envelope" />email</a
+                  >
+                </p>
+                <p v-if="delegate.website">
+                  <a target="_blank" :href="delegate.website"
+                    ><i class="fa fa-globe" />website</a
+                  >
+                </p>
+                <p v-if="delegate.forum">
+                  <a
+                    target="_blank"
+                    :href="
+                      `https://forum.unikname.com/u/${delegate.forum}/summary`
+                    "
+                    ><i class="fa fa-globe" />forum</a
+                  >
+                </p>
+              </div>
+              <div v-else>
+                <p>
+                  <a
+                    target="_blank"
+                    href="https://github.com/unik-name/uns-delegates-website/blob/master/README.md"
+                  >
+                    👉 Claim this delegate profile</a
+                  >
+                </p>
+              </div>
+              <div v-if="!loading" class="description">
+                <p>rank: {{ delegate.rank }} / votes: {{ delegate.votes }}%</p>
+                <span v-if="delegate.forger"
+                  >elected:
+                  <img
+                    :src="require(`@assets/check.svg`)"
+                    height="15px"
+                    width="15px"
+                    alt="status"
+                  />
+                  <p>
+                    status:<span :class="`status-${delegate.isLive}`">{{
+                      delegate.isLive
+                    }}</span>
+                  </p>
+                </span>
+                <span v-else
+                  >elected:
+                  <img
+                    :src="require(`@assets/cross.svg`)"
+                    height="15px"
+                    width="15px"
+                    alt="status"
+                  />
+                </span>
+              </div>
+              <div v-else class="placeholder shimmer">
+                <div class="fake-text medium" />
+                <div class="fake-text short" />
+                <div class="fake-text" />
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
     </span>
-    
-    <script src="https://kit.fontawesome.com/ab9d8096cd.js" crossorigin="anonymous" />
-</div>
+
+    <script
+      src="https://kit.fontawesome.com/ab9d8096cd.js"
+      crossorigin="anonymous"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-    created () {
-        this.$data.delegates = this.$site.headTags.find(el => el[0] === 'delegateData')[1]
-    },
-    async mounted() {
-        const res = await fetch("https://api.uns.network/api/v2/delegates").then(res => res.json())
-        const delegates = this.delegates
-        this.delegates.forEach(delegate => {
-            const data = res.data.find(el => el.username === delegate.unikid)
-            delegate.rank = data.rank
-            delegate.votes = data.production.approval
-            if (delegate.forger) {
-                delegate.isLive = (Date.now() - data.blocks.last.timestamp.unix*1000)/1000 < 600 ? 'active' : 'not active'
-            }
-        })
-        this.loading = false
-    },
-    data() {
-        return {
-            delegates: [],
-            loading: true
-        }
+  created() {
+    const maybeData = this.$site.headTags.find(
+      (el) => el[0] === "delegateData"
+    );
+    if (maybeData && Array.isArray(maybeData) && maybeData.length > 0) {
+      this.$data.delegates = maybeData[1];
+    } else {
+      this.$data.delegates = [];
     }
-}
+  },
+  async mounted() {
+    const res = await fetch(
+      "https://api.uns.network/api/v2/delegates"
+    ).then((res) => res.json());
+    const delegates = this.delegates;
+    this.delegates.forEach((delegate) => {
+      const data = res.data.find((el) => el.username === delegate.unikid);
+      delegate.rank = data.rank;
+      delegate.votes = data.production.approval;
+      if (delegate.forger) {
+        delegate.isLive =
+          (Date.now() - data.blocks.last.timestamp.unix * 1000) / 1000 < 600
+            ? "active"
+            : "not active";
+      }
+    });
+    this.loading = false;
+  },
+  data() {
+    return {
+      delegates: [],
+      loading: true,
+    };
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -115,7 +194,7 @@ export default {
   overflow hidden
   text-align center
   margin 0px 10px 10px 0px
-  background-color #EFF6F6    
+  background-color #EFF6F6
   .card-header
     .not-claimed
         color: red
@@ -174,7 +253,7 @@ export default {
 .unik-badge.unik-badge-network
   background-color: #16c8c0
 
-/* https://codesandbox.io/s/skeleton-placeholder-forked-9i8f7?file=/index.html:1365-1392 */ 
+/* https://codesandbox.io/s/skeleton-placeholder-forked-9i8f7?file=/index.html:1365-1392 */
 .placeholder
         padding: 20px
         max-width: 300px
--- docs/.vuepress/my-plugin.js
+++ docs/.vuepress/my-plugin.js
@@ -1,85 +1,128 @@
-const axios = require('axios')
+const axios = require("axios");
 
 module.exports = (options, context) => {
-  const pages = []
-  let uniks = []
-  let delegates = []
+  const pages = [];
+  let uniks = [];
+  let delegates = [];
 
   return {
     extendPageData($page) {
-      if ($page.regularPath.includes("delegates") && !$page.regularPath.includes("embedded") && !$page.regularPath.includes('_')) {
-        pages.push($page)
+      if (
+        $page.regularPath.includes("delegates") &&
+        !$page.regularPath.includes("embedded") &&
+        !$page.regularPath.includes("_")
+      ) {
+        pages.push($page);
       }
       if ($page.regularPath.includes("embedded")) {
-        const page = pages.find(page => page.regularPath.includes($page.regularPath.split('/')[2]))
+        const page = pages.find((page) =>
+          page.regularPath.includes($page.regularPath.split("/")[2])
+        );
         if (page) {
           for (const [key, value] of Object.entries(page.frontmatter)) {
-            $page.frontmatter[key] = value
+            $page.frontmatter[key] = value;
           }
         }
-        pages.push($page)
+        pages.push($page);
       }
     },
 
-    async additionalPages () {
-      await axios.get('https://api.uns.network/api/v2/delegates').then(res => delegates = res.data.data)
-      const unikids = delegates.filter(delegate => !delegate.username.includes('genesis')).map(delegate => delegate.username)
-      await axios.post('https://api.uns.network/api/v2/uniks/search', {id: unikids}).then(res => uniks = res.data.data)
+    async additionalPages() {
+      delegates = await axios
+        .get("https://api.uns.network/api/v2/delegates")
+        .then((res) => res.data.data);
+      const unikids = delegates
+        .filter((delegate) => !delegate.username.includes("genesis"))
+        .map((delegate) => delegate.username);
+      uniks = await axios
+        .post("https://api.uns.network/api/v2/uniks/search", { id: unikids })
+        .then((res) => res.data.data);
 
-      const additionalPages = pages.map(page => {return {path: `${page.regularPath}embedded.html`, content: page._strippedContent}})
-      unikids.forEach(unikid => {
-        if (!pages.find(page => page.regularPath.includes(unikid))) {
-          additionalPages.push({path: `/delegates/${unikid}/`, content: '\n## Summary\n\n## Contributions\n'})
-          additionalPages.push({path: `/delegates/${unikid}/embedded.html`, content: '\n## Summary\n\n## Contributions\n'})
+      const additionalPages = pages.map((page) => {
+        return {
+          path: `${page.regularPath}embedded.html`,
+          content: page._strippedContent,
+        };
+      });
+      unikids.forEach((unikid) => {
+        if (!pages.find((page) => page.regularPath.includes(unikid))) {
+          additionalPages.push({
+            path: `/delegates/${unikid}/`,
+            content: "\n## Summary\n\n## Contributions\n",
+          });
+          additionalPages.push({
+            path: `/delegates/${unikid}/embedded.html`,
+            content: "\n## Summary\n\n## Contributions\n",
+          });
         }
-      })
-      return additionalPages
+      });
+      return additionalPages;
     },
 
-    async ready() {
+    async ready() {
       const { siteConfig } = context;
       if (!siteConfig.head) siteConfig.head = [];
-      pages.forEach(page => {
-        const unik = uniks.find(unik => unik.id === page.regularPath.split('/')[2])
-        const delegate = delegates.find(delegate => delegate.username === page.regularPath.split('/')[2])
+      pages.forEach((page) => {
+        const unik = uniks.find(
+          (unik) => unik.id === page.regularPath.split("/")[2]
+        );
+        const delegate = delegates.find(
+          (delegate) => delegate.username === page.regularPath.split("/")[2]
+        );
+
+        // Allow to generate pages for delegate without contribution
         if (Object.keys(page.frontmatter).length === 0) {
-          page.frontmatter = {
-            unikid: page.regularPath.split('/')[2],
-            ownerId: unik.ownerId,
-            unikname: unik.defaultExplicitValue,
+          page.frontmatter = {};
+        }
+
+        if (page.frontmatter.unikid) {
+          page.frontmatter.notCompleted = false;
+        } else {
+          page.frontmatter.notCompleted = true;
+          page.frontmatter.unikid = page.regularPath.split("/")[2];
+        }
+
+        page.frontmatter.ownerId = unik.ownerId;
+        page.frontmatter.unikname = unik.defaultExplicitValue;
+        page.frontmatter.type = delegate.type;
+        page.frontmatter.forger = delegate.rank < 24 ? true : false;
+
+        if (page.regularPath.includes("embedded")) {
+          page.frontmatter.layout = "DelegateLayout";
+          page.frontmatter.format = "embedded";
+        } else {
+          page.frontmatter.format = "complete";
+        }
+      });
+
+      const pagesWithoutFrontmatter = pages.filter(
+        (page) => Object.keys(page.frontmatter).length === 0
+      );
+      pagesWithoutFrontmatter.forEach(
+        (page) =>
+          (page.frontmatter = {
+            unikid: page.regularPath.split("/")[2],
             notCompleted: true,
-            type: delegate.type,
-            forger: delegate.rank < 24 ? true : false
-          }
-        } else {
-          page.frontmatter.ownerId = unik.ownerId
-          page.frontmatter.unikname = unik.defaultExplicitValue,
-          page.frontmatter.notCompleted =  false,
-          page.frontmatter.type = delegate.type
-          page.frontmatter.forger = delegate.rank < 24 ? true : false
-        }
-        if (page.regularPath.includes('embedded')) {
-          page.frontmatter.layout = 'DelegateLayout'
-          page.frontmatter.format = 'embedded'
-        } else {
-          page.frontmatter.format = 'complete'
-        }
-      })
+          })
+      );
+      const delegatesPages = pages.filter(
+        (page) => !page.regularPath.includes("embedded")
+      );
 
-      const pagesWithoutFrontmatter = pages.filter(page => Object.keys(page.frontmatter).length === 0)
-      pagesWithoutFrontmatter.forEach(page => page.frontmatter = {unikid: page.regularPath.split('/')[2], notCompleted: true})
-      const delegatesPages = pages.filter(page => !page.regularPath.includes('embedded'))
-
-      let finalData = delegatesPages.map(page => page.frontmatter)
+      let finalData = delegatesPages.map((page) => page.frontmatter);
 
       // keep only delegates of type individual
-      finalData = finalData.filter(delegate => delegate.type === 'individual')
+      finalData = finalData.filter(
+        (delegate) => delegate.type === "individual"
+      );
       // sort by forger status and alphabetically
-      finalData = finalData.sort((a, b) => b.forger - a.forger || a.unikname.localeCompare(b.unikname))
+      finalData = finalData.sort(
+        (a, b) => b.forger - a.forger || a.unikname.localeCompare(b.unikname)
+      );
       // sort if delegate completed his profile
-      finalData = finalData.sort((a, b) => a.notCompleted - b.notCompleted)
+      finalData = finalData.sort((a, b) => a.notCompleted - b.notCompleted);
 
-      siteConfig.head.push(['delegateData', finalData])
-    }
-  }
-}
+      siteConfig.head.push(["delegateData", finalData]);
+    },
+  };
+};
